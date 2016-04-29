8b4a95d1b294568179db21cc974b0b54024a8bb5
Hernan Y.Ke <yuriqiao@gmail.com>
2016-04-29 18:50:53 -0400

    custom date-parsing sorter

--- assets/design_the_web.js
+++ assets/design_the_web.js
@@ -1,7 +1,18 @@
 $(function(){
+	$.tablesorter.addParser({
+		id:'getDateAttr',
+		is:function(sort){
+			return false;
+		},
+		format: function(sort,table,cell,cellIndex){
+			return $(cell).attr('data-date');
+		},
+		type: 'text'
+	})
 $('table#courses').tablesorter({
-	sortList:[[2,0]],
+	sortList:[[3,0]],
 	cssAsc: 'sorted asc',
-	cssDesc:'sorted desc'
-})
+	cssDesc:'sorted desc',
+	headers:{3 : {sorter:'getDateAttr'}}
+});
 })
